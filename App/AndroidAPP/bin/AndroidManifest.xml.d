81eebfc1cf051f8aaee054a6cb6112849dbcae4a
JumpBean <jumpbeandev@163.com>
2017-02-22 23:38:06 +0800

    delete useless files

--- App/AndroidAPP/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\bin\AndroidManifest.xml : \
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\AndroidManifest.xml \
--- App/AndroidAPP/bin/MainActivity-release-unsigned.apk.d
+++
@@ -1,5 +0,0 @@
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\bin\MainActivity-release-unsigned.apk : \
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\bin\MainActivity.ap_ \
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\bin\classes.dex \
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\libs\android-support-v4.jar \
-D:\DocBook\SVN Root\RapidApp\istore\master\App\AndroidAPP\libs\json.jar \
